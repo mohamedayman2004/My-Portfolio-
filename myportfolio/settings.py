ffb2318f967246864a540db66b1025e2acea5f9e
MohamedAyman <mohamedaymanessawi2004@gmail.com>
2026-05-07 23:27:02 +0300

    Revert Cloudinary - User prefers easier route

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -42,9 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'main',
 ]
 
@@ -146,14 +144,7 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Cloudinary Integration for permanent image storage
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
-}
-
-# Only use Cloudinary if credentials are provided (i.e. in Production)
-if os.getenv('CLOUDINARY_CLOUD_NAME'):
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# This is essential for handling uploads on Render
+# If not using AWS S3, at least tell Django where to put them, but remember Render free tier deletes files on restart
+# To avoid deletion, we use WhiteNoise for Static, but Media needs explicit handling
 
