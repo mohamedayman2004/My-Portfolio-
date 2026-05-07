8a5e184ed042874ffd842693b0cdb2c1c5fed891
MohamedAyman <mohamedaymanessawi2004@gmail.com>
2026-05-07 20:34:23 +0300

    Force Django to serve media files in production

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -15,15 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# This allows images to load while you are developing
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
